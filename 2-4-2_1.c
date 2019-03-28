7f8366409ffe66d55bc532a646c1f12bd304aaa6
Eric <1253120691@qq.com>
2019-03-28 21:01:26 +0800

    update

---
+++ 2-4-2_1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+    int n1,n2,result;
+    printf("please input the two number:\n");
+    scanf("%d%d",&n1,&n2);
+    result = n1* n2;
+    printf("the result is :%d\n",result);
+    return 0;
+}
